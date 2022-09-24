27a5dacec59282e854ef29256b1a7a05181c19ce
aleino0 <113008053+aleino0@users.noreply.github.com>
2022-09-24 23:26:14 +0200

    Add files via upload

---
+++ Prvi-Window-projekt-main/Prvi-Window-projekt-main/MojaPrvaWindowsAplikacija/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MojaPrvaWindowsAplikacija
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UnosbrojaA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Izracun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int brojA, brojB, zbroj;
+
+                brojA = Convert.ToInt32(txtUnosbrojaA.Text);
+                brojB = Convert.ToInt32(txtUnosbrojaB.Text);
+                zbroj = brojA + brojB;
+
+                txtIspis.Text = "" + zbroj;
+            }
+
+            catch (FormatException)
+            {
+                txtIspis.Text = "";
+                MessageBox.Show("Moraju biti upisana dva broja!","Greška u upisu");
+            }
+            
+            
+        }
+
+        private void txtIspis_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
